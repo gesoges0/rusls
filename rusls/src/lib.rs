295526eb377c9e395cf05ebd6bfa595047d62c1c
gesoges0 <elasticnet1995@gmail.com>
2024-02-12 17:33:36 +0900

    fix: format

--- rusls/src/lib.rs
+++ rusls/src/lib.rs
@@ -1,12 +1,14 @@
 use clap::{App, Arg};
 use std::error::Error;
+use std::path::PathBuf;
+use tabular::{Row, Table};
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
 #[derive(Debug)]
 pub struct Config {
-    paths: Vec<String>,  // ファイルとディレクトリを表す
-    long: bool,  // 長形式で表示するか？
+    paths: Vec<String>, // ファイルとディレクトリを表す
+    long: bool,         // 長形式で表示するか？
     show_hidden: bool,  // エントリを表示する？
 }
 
@@ -34,7 +36,7 @@ pub fn get_args() -> MyResult<Config> {
                 .takes_value(false)
                 .help("Show all fiels")
                 .short("a")
-                .long("all")
+                .long("all"),
         )
         .get_matches();
 
@@ -45,9 +47,115 @@ pub fn get_args() -> MyResult<Config> {
     })
 }
 
+fn format_output(paths: &[PathBuf]) -> MyResult<String> {
+    //          1   2     3     4     5     6     7     8
+    let fmt = "{:<}{:<}  {:>}  {:<}  {:<}  {:>}  {:<}  {:<}";
+    let mut table = Table::new(fmt);
+
+    for path in paths {
+        table.add_row(
+            Row::new()
+                .with_cell("") // 1
+                .with_cell("") // 2
+                .with_cell("") // 3
+                .with_cell("") // 4
+                .with_cell("") // 5
+                .with_cell("") // 6
+                .with_cell("") // 7
+                .with_cell(""), // 8
+        );
+    }
+    OK(format!("{}", table))
+}
+
 pub fn run(config: Config) -> MyResult<()> {
-    println!("{:?}", config);
+    let paths = find_files(&config.paths, config.show_hidden)?;
+    for path in paths {
+        println!("{}", path.display());
+    }
     Ok(())
 }
 
+fn find_files(paths: &[String], show_hidden: bool) -> MyResult<Vec<PathBuf>> {
+    unimplemented!();
+}
 
+#[cfg(test)]
+mod test {
+    use super::find_files;
+
+    #[test]
+    fn test_find_files() {
+        // ディレクトリにある隠しエントリ以外のエントリを検索する
+        let res = find_files(&["tests/inputs".to_string()], false);
+        assert!(res.is_ok());
+        let mut filenames: Vec<_> = res
+            .unwrap()
+            .iter()
+            .map(|entry| entry.display().to_string())
+            .collect();
+        filenames.sort();
+        assert_eq!(
+            filenames,
+            [
+                "tests/inputs/bustle.txt",
+                "tests/inputs/dir",
+                "tests/inputs/empty.txt",
+                "tests/inputs/fox.txt",
+            ]
+        );
+
+        // 存在するファイルは隠しファイルであっても検索出来るようにする
+        let res = find_files(&["tests/inputs/.hidden".to_string()], false);
+        assert!(res.is_ok());
+        let filenames: Vec<_> = res
+            .unwrap()
+            .iter()
+            .map(|entry| entry.display().to_string())
+            .collect();
+        assert_eq!(filenames, ["tests/inputs/.hidden"]);
+
+        // 複数のパスを与えてテストする
+        let res = find_files(
+            &[
+                "tests/inputs/bustle.txt".to_string(),
+                "tests/inputs/dir".to_string(),
+            ],
+            false,
+        );
+        assert!(res.is_ok());
+        let mut filenames: Vec<_> = res
+            .unwrap()
+            .iter()
+            .map(|entry| entry.display().to_string())
+            .collect();
+        filenames.sort();
+        assert_eq!(
+            filenames,
+            ["tests/inputs/bustle.txt", "tests/inputs/dir/spiders.txt"]
+        );
+    }
+
+    #[test]
+    fn test_find_files_hidden() {
+        // ディレクトリにあるすべてのエントリを検索する
+        let res = find_files(&["tests/inputs".to_string()], true);
+        assert!(res.is_ok());
+        let mut filenames: Vec<_> = res
+            .unwrap()
+            .iter()
+            .map(|entry| entry.display().to_string())
+            .collect();
+        filenames.sort();
+        assert_eq!(
+            filenames,
+            [
+                "tests/inputs/.hidden",
+                "tests/inputs/bustle.txt",
+                "tests/inputs/dir",
+                "tests/inputs/empty.txt",
+                "tests/inputs/fox.txt",
+            ]
+        );
+    }
+}
